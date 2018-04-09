1e95007e31ad70ff45bcd117dc7e59b657946458
Siddharth Venkatesan <siddvenk@umich.edu>
2018-04-08 21:08:33 -0400

    Finished making test/training files

--- make_test_files.py
+++ make_test_files.py
@@ -2,3 +2,23 @@
 
 import os
 
+if __name__ == "__main__":
+	cwd = os.getcwd()
+	playlistFile = cwd + '/Playlist_hypergraph.txt'
+	outputFolder = cwd + '/testStartingIds'
+	testPlaylists = {}
+	with open(playlistFile, 'r') as f:
+		for line in f:
+			track_id, playlist = line.split()
+			if int(playlist) > 699:
+				if playlist not in testPlaylists:
+					testPlaylists[playlist] = []
+				testPlaylists[playlist].append(track_id)
+	f.close()
+
+	for key, value in testPlaylists.items():
+		outputFile = outputFolder + '/' + str(key)
+		with open(outputFile, 'w+') as f:
+			for node in value:
+				f.write("{0}\n".format(node))
+		f.close()
